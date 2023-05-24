e12c1fb4a53a159fd6c60b40037c36a3f18a8065
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-05-24 10:32:20 -0400

    Pokemon names are appearing 20 at a time

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { useState } from "react";
+import PokemonList from "./PokemonList"
+import axios from "axios";
 
 function App() {
-  return null
+  const [pokemon, setPokemon] = useState([])
+
+  axios.get("https://pokeapi.co/api/v2/pokemon/").then(res => {
+    setPokemon(res.data.results.map(p => p.name))
+  }) 
+  return (
+    <PokemonList pokemon={pokemon}/>
+  )
 }
 export default App;
